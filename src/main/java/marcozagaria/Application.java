1e6c394d9de3a0925436e45b59d7bc362d219b6b
Marco Zagaria <marcozagaria@live.com>
2024-10-04 12:18:13 +0200

    metodo aggiungi un gioco

--- src/main/java/marcozagaria/Application.java
+++ src/main/java/marcozagaria/Application.java
@@ -1,20 +1,18 @@
 package marcozagaria;
 
-import marcozagaria.Classi.Gioco;
+import marcozagaria.Classi.Collezione;
 import marcozagaria.Classi.GiocoDaTavolo;
 import marcozagaria.Classi.Videogioco;
 import marcozagaria.Enum.Genere;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 public class Application {
 
-    public static void main(String[] args) {
-        List<Gioco> giochilist = new ArrayList<>();
-        giochilist.add(new Videogioco(1, 50, new Date(2024, 10, 11), "Dragon Ball Sparking 0", "xbox", 100, Genere.AZIONE));
-        giochilist.add(new GiocoDaTavolo(2, 25.9, new Date(2024, 5, 17), "Munchkin", 5, 2));
+    public static void main(String[] args) throws Exception {
+        Collezione giochilist = new Collezione();
+        giochilist.agiungiGioco(new Videogioco(1, 50, new Date(2024, 10, 11), "Dragon Ball Sparking 0", "xbox", 100, Genere.AZIONE));
+        giochilist.agiungiGioco(new GiocoDaTavolo(2, 25.9, new Date(2020, 5, 17), "Munchkin", 5, 2));
         System.out.println(giochilist);
     }
 
--- src/main/java/marcozagaria/Classi/Collezione.java
+++ src/main/java/marcozagaria/Classi/Collezione.java
@@ -1,11 +1,34 @@
 package marcozagaria.Classi;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Collezione {
-    protected List<Gioco> giochi;
+    protected List<Gioco> giochilist;
 
-    public Collezione(List<Gioco> giochi) {
-        this.giochi = giochi;
+    public Collezione() {
+        this.giochilist = new ArrayList<>();
+    }
+
+    public List<Gioco> getGiochi() {
+        return giochilist;
+    }
+
+    public void setGiochi(List<Gioco> giochi) {
+        this.giochilist = giochi;
+    }
+
+    public void agiungiGioco(Gioco gioco) throws Exception {
+        if (giochilist.stream().anyMatch(gioco1 -> gioco1.getIdGioco() == gioco.getIdGioco())) {
+            throw new Exception("gioco con id " + gioco.getIdGioco() + " gia esistente");
+        }
+        giochilist.add(gioco);
+    }
+
+    @Override
+    public String toString() {
+        return "Collezione{" +
+                "giochilist=" + giochilist +
+                '}';
     }
 }
